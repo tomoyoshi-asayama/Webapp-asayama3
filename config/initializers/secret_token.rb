e128d291fc1653d1417c534a194793cefb95c609
tomoyoshi-asayama <tomoyoshi.asayama@gmail.com>
2013-04-17 21:33:12 +0900

    commit end

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebappAsayama3::Application.config.secret_token = '28cb0905a063dcb2081b0c0502191dc228e09c55b1fccd10324e32197c6f731ffde352c3f7588424e1a904bcc0eb4e429a15a0e1ca05508eb447f738753466a3'
